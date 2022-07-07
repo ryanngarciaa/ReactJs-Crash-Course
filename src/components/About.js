4db6d0785faf3321a367f8ba6f47ce812c590d5e
ryanngarciaa <36841478+ryanngarciaa@users.noreply.github.com>
2022-07-07 21:37:15 +0800

    Latest Changes

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0.0 Test ReactJs crash course task tracker.</h4>
+      <Link to='/'>Back</Link>
+    </div>
+  );
+};
+
+export default About;
